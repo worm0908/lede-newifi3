dba030276be45aed1f3b5ab0e9bf020ef19431c9
SGMC <7271773+worm0908@users.noreply.github.com>
2020-09-05 12:30:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,12 +10,17 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default options
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/RT-HOME/g' package/base-files/files/bin/config_generate
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/\+luci-theme-bootstrap/+luci-theme-atmaterial/g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/atmaterial_Brown/g' feeds/luci/modules/luci-base/root/etc/config/luci
+# 添加atmaterial主题
+git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+mv package/kenzok8/luci-theme-atmaterial package/lean
+rm -rf package/kenzok8
 
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+# 修改默认配置
+sed -i -e 's/192.168.1.1/192.168.5.1/' -e 's/OpenWrt/RT-DF/' package/base-files/files/bin/config_generate
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/\+luci-theme-bootstrap/+luci-theme-atmaterial/' feeds/luci/collections/luci/Makefile
+sed -i 's/mediaurlbase \/luci-static\/bootstrap/mediaurlbase \/luci-static\/atmaterial_Brown/' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 添加Hellow world
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
