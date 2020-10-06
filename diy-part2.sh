ba90c39e3f515ce272e1f8fa8812b793136a3b56
SGMC <7271773+worm0908@users.noreply.github.com>
2020-10-06 22:21:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,18 +16,21 @@
 #mv package/kenzok8/luci-theme-atmaterial package/lean
 #rm -rf package/kenzok8
 
-# Passwall from kenzok8
-[ -e package/openwrt-packages ] && rm -rf package/openwrt-packages
-[ -e package/small ] && rm -rf package/small
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/kenzok8/small package/small
-
 # 添加lua-maxminddb依赖'
 #[ -e package/lean/lua-maxminddb ] && rm -rf package/lean/lua-maxminddb
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 # 添加Hellow world'
 #[ -e package/lean/luci-app-vssr ] && rm -rf package/lean/luci-app-vssr
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+
+# 更改 /lib/functions/service.sh，修正vssr启动kcptun问题
+#sed -i 's/$ssd${1:+\ --\ "$@"}/$ssd${1:+ -- ${@}}/' package/base-files/files/lib/functions/service.sh
+
+# Passwall from kenzok8
+[ -e package/openwrt-packages ] && rm -rf package/openwrt-packages
+[ -e package/small ] && rm -rf package/small
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
 
 # 更改默认设置'
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
@@ -37,6 +40,3 @@ sed -i 's/\+luci-theme-bootstrap/+luci-theme-atmaterial/g' feeds/luci/collection
 sed -i 's/bootstrap/atmaterial_Brown/g' feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i 's~$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.~$1$5p2C0aMa$F75x3a/k3vM24iMXmb1d71~' package/lean/default-settings/files/zzz-default-settings
 sed -i -e '/echo.+authorized_keys$/,+1d' -e "/wgetrc$/a\\\\necho 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDikWAn\\/vJiuyknfrq\\/cfxtmLxbCulfgdV67TKVHV5b5CyOkt2z7ycSPy90ige3KiFpP6OAQ\\/VTv6eL7hALUtx0+vdesYDJah6DgHGRQNt+FNC5jl6u1BnViGgnu3rGcq+l6scK0213VifZMYNqks3IkOhR11dWhapTXgHVbuCPsr8uGJSwDlPs4jCBhPTN5VCC9vuDSKR4KBKw33YDY\\/fN778G6ReDezLWe3d3xVjGGD5yKJxoVgwsHFyjCwAzwkbhdaFPZLFZgXL+WWiLSwqUsgHKpebL7uDuqHRtIBDkAindx+p+d81+5ZIWX6NWMwebI8KhsR7NHOgiNbwVdN9D Newifi3' > \/etc\/dropbear\/authorized_keys" package/lean/default-settings/files/zzz-default-settings
-
-# 更改 /lib/functions/service.sh，修正vssr启动kcptun问题
-#sed -i 's/$ssd${1:+\ --\ "$@"}/$ssd${1:+ -- ${@}}/' package/base-files/files/lib/functions/service.sh
