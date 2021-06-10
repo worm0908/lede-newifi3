d3faac5c2d9cb7fe3de8b7071c50093edbf2011e
SGMC <7271773+worm0908@users.noreply.github.com>
2021-06-10 23:44:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Init a feed source
-sed -i '12,$d' feeds.conf.default
-
 # Add a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+if [[ `grep -qiE "kenzo8" feeds.conf.default -eq 1 ]] ; then
+  sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+fi
